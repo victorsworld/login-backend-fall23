0591404c3fb79f51bbfe29033563f763f36de1f3
Michael Dwyer <michael.dwyer@codeimmersives.com>
2023-09-26 10:05:29 -0700

    made login-test route

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Users index route');
 });
+
+router.get('/login-test', function(req,res) {
+  console.log(req.body);
+  res.send({
+    username: req.body.username
+  })
+})
 
 module.exports = router;
